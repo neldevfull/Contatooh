486bbbe30d71df79cd8a53e82cb9af43b28b27c6
neldevfull <nfrugeri@gmail.com>
2015-09-07 11:25:14 +0000

    -- Implementation of the method-override in ExpressJS middleware server to support the Delete and Put verbs --

--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 //config/express.js
-var express = require('express');
-var load    = require('express-load');
+var express    = require('express');
+var load       = require('express-load');
+var bodyParser = require('body-parser'); 
 
 module.exports = function() { 
     var app = express();
@@ -11,6 +12,10 @@ module.exports = function() {
     // Template Engine
     app.set('view engine', 'ejs');
     app.set('views', './app/views');
+    // Middleware to parser http verbs
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use(require('method-override')());
     // Loaded modules and its dependencies 
     load('models', {cwd: 'app'})
     	.then('controllers')
